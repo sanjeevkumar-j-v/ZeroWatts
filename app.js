6c2ea943de9e1d0de1baa5a06658e2b8a9077a45
bhuvaneshwara raja <bhuvaneshraja113@gmail.com>
2020-08-20 16:04:03 +0530

    initial-7

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const ejs = require("ejs")
 const bodyParser = require("body-parser");
 const app = express();
 
+var http = require("http");
+var request = require('request');
+var hackerEarth = require("hackerearth-node");
+var hackerEarth = new hackerEarth('c79c38f47874fc0f7d2fa763b4fdf7b31d44fdf7');
+
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
@@ -13,5 +18,61 @@ app.listen(process.env.PORT || 3000, (req,res) => {
 
 app.get("/home", (req,res) => {
 
-  res.render("first");
+  res.render("firstpage", {test:"",result:""});
+})
+app.post("/evaluate",(req,res)=>{
+      console.log(req.body.name);
+      var config={};
+        config.time_limit=1;
+config.memory_limit=323244;
+config.source='print("hello world")';
+config.input="";
+config.language="Py";
+hackerEarth.compile(config,function(err,response){
+      if(err) {
+        //deal with error
+        console.log(err);
+      } else {
+        //deal with response
+        console.log(response);
+      }
+});
+hackerEarth.run(config,function(err,response){
+      if(err) {
+        //deal with error
+        console.log(err);
+      } else {
+        //deal with response
+        console.log(response);
+      }
+});
+  /*var program = {
+      script :req.body.name,
+      language: "python3",
+      versionIndex: "0",
+      stdin:`6
+             7
+             8
+             5`,
+      clientId: "d4a0dc2c72a81f90c50d2257c31f19c7",
+      clientSecret:"7f0485032358e87c5271a349248f1719b7dd05739763aea17679f81b4e6b4520"
+  };
+  request({
+      url: 'https://api.jdoodle.com/v1/execute',
+      method: "POST",
+      json: program
+  },
+
+function (error, response, body, program) {
+      console.log('error:', error);
+      console.log('statusCode:', response && response.statusCode);
+      console.log('body:', body);
+      res.render("firstpage", {test:req.body.name, result:body.output});
+  });*/
+
+
+
+})
+app.get("/landing", (req,res)=>{
+  res.render("landing")
 })
