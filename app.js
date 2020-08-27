9a6d6ac7640f0ad66b4ddb344cfe93b5652ae245
bhuvaneshwara raja <bhuvaneshraja113@gmail.com>
2020-08-27 19:38:47 +0530

    stage-1

--- app.js
+++ app.js
@@ -9,91 +9,50 @@ var hackerEarth = require("hackerearth-node");
 var hackerEarth = new hackerEarth('c79c38f47874fc0f7d2fa763b4fdf7b31d44fdf7');
 
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.use(express.static("public"));
 app.use(bodyParser.json());
-app.listen(process.env.PORT || 3000, (req,res) => {
-  console.log("server start at port 3000")
+app.listen(process.env.PORT || 3000, (req, res) => {
+    console.log("server start at port 3000")
 })
 
-app.get("/home", (req,res) => {
-
-  res.render("firstpage");
+app.get("/codearea", (req, res) => {
+    res.render("codearea");
 })
-app.post("/home",(req,res)=>{
-  console.log(req.body.code);
+app.post("/codearea", (req, res) => {
+    console.log(req.body.code);
+    var code = req.body.code;
+    var lan = req.body.lantype;
+    console.log(lan);
+    var config = {};
+    config.time_limit = 1;
+    config.memory_limit = 323244;
+    config.source = code;
+    config.input = "";
+    config.language = "Py";
+    hackerEarth.compile(config, function(err, response) {
+        if (err) {
+            console.log(err);
+        } else {
 
-     var code = req.body.code;
-      var lan = req.body.lantype;
-      console.log(lan);
-     var config={};
-        config.time_limit=1;
-config.memory_limit=323244;
-config.source=code;
-config.input="";
-config.language="Py";
-hackerEarth.compile(config,function(err,response){
-      if(err) {
-        //deal with error
-        console.log(err);
-      } else {
-        //deal with response
-
-      }
-});
-hackerEarth.run(config,function(err,response){
-      if(err) {
-        //deal with error
-        console.log(err);
-      } else {
-        //deal with response
-        var a = JSON.parse(response)
-        console.log(a);
-        if(a.compile_status === 'OK'){
-          res.send([a.run_status.output,"green"]);
         }
-        else if(a.compile_status !== 'OK'){
-          res.send([a.compile_status,"red"]);
+    });
+    hackerEarth.run(config, function(err, response) {
+        if (err) {
+            console.log(err);
+        } else {
+            var a = JSON.parse(response)
+            console.log(a);
+            if (a.compile_status === 'OK') {
+                res.send([a.run_status.output, "green"]);
+            } else if (a.compile_status !== 'OK') {
+                res.send([a.compile_status, "red"]);
+            }
         }
-      }
-});
-
-
-  /*var program = {
-      script :req.body.name,
-      language: "python3",
-      versionIndex: "0",
-      stdin:`6
-             7
-             8
-             5`,
-      clientId: "d4a0dc2c72a81f90c50d2257c31f19c7",
-      clientSecret:"7f0485032358e87c5271a349248f1719b7dd05739763aea17679f81b4e6b4520"
-  };
-  request({
-      url: 'https://api.jdoodle.com/v1/execute',
-      method: "POST",
-      json: program
-  },
-
-function (error, response, body, program) {
-      console.log('error:', error);
-      console.log('statusCode:', response && response.statusCode);
-      console.log('body:', body);
-      res.render("firstpage", {test:req.body.name, result:body.output});
-  });*/
-
-
-
+    });
 })
-app.get("/landing", (req,res)=>{
-  res.render("landing")
-})
-app.get("/test", (req,res) => {
-  res.render("testpage");
-})
-app.post("/test", (req,res) =>{
-  var result = "3";
-  console.log(req.body.name);
-  res.send(result)
+app.get("/landing", (req, res) => {
+    res.render("landing")
 })
---
+++ public/js/index.js
@@ -0,0 +1,68 @@
+$(".content-area .inner-tabs a").click(function() {
+    $(".content-area .inner-tabs a").css("text-decoration", "none");
+    $(this).css("text-decoration", "underline");
+})
+var a = document.querySelectorAll(".content-area .inner-tabs a");
+var b = document.querySelectorAll(".content-area .cont");
+for (let i = 0; i < a.length; i++) {
+    a[i].addEventListener("click", function() {
+        console.log(i);
+        for (var j = 0; j < b.length; j++) {
+            if (i !== j) {
+                b[j].style.display = "none"
+                console.log(j);
+            } else {
+                b[i].style.display = "block"
+                a[i].style.backgroundColor = "lighublue"
+            }
+        }
+    })
+}
+$("form").on('submit', function(e) {
+    e.preventDefault();
+    var codeinp = $(".edit");
+    console.log($(".edit").val());
+    $.ajax({
+        url: '/codearea',
+        method: 'POST',
+        contentType: 'application/json',
+        data: JSON.stringify({
+            code: codeinp.val()
+        }),
+        success: function(response) {
+            if (response[1] === "green") {
+                document.getElementById("default-clr").style.backgroundColor = "rgba(0,128,0,.2)";
+                document.getElementById("default-clr").style.border = "1px solid rgba(0,128,0)";
+                document.querySelector(".op-error p").innerText = response[0];
+            } else if (response[1] === "red") {
+                document.getElementById("default-clr").style.backgroundColor = "rgba(128,0,0,.2)";
+                document.getElementById("default-clr").style.border = "1px solid rgba(128,0,0)";
+                document.querySelector(".op-error p").innerText = response[0];
+            }
+        }
+    })
+})
+
+var editor = ace.edit("editor");
+editor.setTheme("ace/theme/cobalt");
+editor.session.setMode("ace/mode/python");
+ace.edit(element, {
+    mode: "ace/mode/python",
+    selectionStyle: "text"
+})
+
+function Customof() {
+    var st = document.querySelector(".lanselect").value;
+    var fon = document.querySelector(".font").value;
+    var theme = document.querySelector(".themeselect").value;
+    console.log(theme);
+    editor.setTheme("ace/theme/" + theme);
+    editor.session.setMode("ace/mode/" + st);
+    document.querySelector('#editor').style.fontSize = fon;
+
+}
+
+function GetVal() {
+    var s = document.querySelector(".ace_content").innerText;
+    console.log(document.querySelector(".edit").value = s);
+}
