5d0fb5703524e350527edc0cf9dc970e0470e69b
bhuvaneshwara raja <bhuvaneshraja113@gmail.com>
2020-08-27 13:05:06 +0530

    initial-8

--- app.js
+++ app.js
@@ -11,21 +11,25 @@ var hackerEarth = new hackerEarth('c79c38f47874fc0f7d2fa763b4fdf7b31d44fdf7');
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
-
+app.use(bodyParser.json());
 app.listen(process.env.PORT || 3000, (req,res) => {
   console.log("server start at port 3000")
 })
 
 app.get("/home", (req,res) => {
 
-  res.render("firstpage", {test:"",result:""});
+  res.render("firstpage");
 })
-app.post("/evaluate",(req,res)=>{
-      console.log(req.body.name);
-      var config={};
+app.post("/home",(req,res)=>{
+  console.log(req.body.code);
+
+     var code = req.body.code;
+      var lan = req.body.lantype;
+      console.log(lan);
+     var config={};
         config.time_limit=1;
 config.memory_limit=323244;
-config.source='print("hello world")';
+config.source=code;
 config.input="";
 config.language="Py";
 hackerEarth.compile(config,function(err,response){
@@ -34,7 +38,7 @@ hackerEarth.compile(config,function(err,response){
         console.log(err);
       } else {
         //deal with response
-        console.log(response);
+
       }
 });
 hackerEarth.run(config,function(err,response){
@@ -43,9 +47,18 @@ hackerEarth.run(config,function(err,response){
         console.log(err);
       } else {
         //deal with response
-        console.log(response);
+        var a = JSON.parse(response)
+        console.log(a);
+        if(a.compile_status === 'OK'){
+          res.send([a.run_status.output,"green"]);
+        }
+        else{
+          res.send([a.compile_status,"red"]);
+        }
       }
 });
+
+
   /*var program = {
       script :req.body.name,
       language: "python3",
@@ -75,4 +88,12 @@ function (error, response, body, program) {
 })
 app.get("/landing", (req,res)=>{
   res.render("landing")
+})
+app.get("/test", (req,res) => {
+  res.render("testpage");
+})
+app.post("/test", (req,res) =>{
+  var result = "3";
+  console.log(req.body.name);
+  res.send(result)
 })
