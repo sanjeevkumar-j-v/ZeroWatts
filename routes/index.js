694e62163f43cfc97d8402e4c996ae77c1278531
sanjeevkumar-j-v <sanjeevkumarjv@gmail.com>
2020-08-28 16:26:00 +0530

    Use ejs variable

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    res.render("landing")
+    res.render("landing", {
+        "title": "ZeroWatts"
+    })
 })
 
 router.use("/codearea", require("./codearea"))
