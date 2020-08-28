4d3785d2cec6d62d64211ba300451e4228106c6b
sanjeevkumar-j-v <sanjeevkumarjv@gmail.com>
2020-08-28 16:14:10 +0530

    Remove the now ignored directory node_modules

---
+++ routes/codearea.js
@@ -0,0 +1,42 @@
+const express = require("express");
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+    res.render("codearea");
+})
+
+router.post("/", (req, res) => {
+    console.log(req.body.code);
+    var code = req.body.code;
+    var lan = req.body.lantype;
+    console.log(lan);
+    var config = {};
+    config.time_limit = 1;
+    config.memory_limit = 323244;
+    config.source = code;
+    config.input = "";
+    config.language = "Py";
+    hackerEarth.compile(config, function(err, response) {
+        if (err) {
+            console.log(err);
+        } else {
+
+        }
+    });
+    hackerEarth.run(config, function(err, response) {
+        if (err) {
+            console.log(err);
+        } else {
+            var a = JSON.parse(response)
+            console.log(a);
+            if (a.compile_status === 'OK') {
+                res.send([a.run_status.output, "green"]);
+            } else if (a.compile_status !== 'OK') {
+                res.send([a.compile_status, "red"]);
+            }
+        }
+    });
+})
+
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -2,45 +2,11 @@ const express = require("express");
 
 const router = express.Router();
 
-router.get("/codearea", (req, res) => {
-    res.render("codearea");
-})
-router.post("/codearea", (req, res) => {
-    console.log(req.body.code);
-    var code = req.body.code;
-    var lan = req.body.lantype;
-    console.log(lan);
-    var config = {};
-    config.time_limit = 1;
-    config.memory_limit = 323244;
-    config.source = code;
-    config.input = "";
-    config.language = "Py";
-    hackerEarth.compile(config, function(err, response) {
-        if (err) {
-            console.log(err);
-        } else {
-
-        }
-    });
-    hackerEarth.run(config, function(err, response) {
-        if (err) {
-            console.log(err);
-        } else {
-            var a = JSON.parse(response)
-            console.log(a);
-            if (a.compile_status === 'OK') {
-                res.send([a.run_status.output, "green"]);
-            } else if (a.compile_status !== 'OK') {
-                res.send([a.compile_status, "red"]);
-            }
-        }
-    });
-})
-router.get("/landing", (req, res) => {
+router.get("/", (req, res) => {
     res.render("landing")
 })
 
+router.use("/codearea", require("./codearea"))
 
 
 module.exports = router;
